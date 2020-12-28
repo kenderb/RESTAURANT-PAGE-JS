345626e96bfe7da7da76e5b9e5d6f602577f479a
kenderb <40863532+kenderb@users.noreply.github.com>
2020-12-28 17:31:23 -0500

    Add history content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import homePage from './home';
 import navButton from './button-actions';
 import menuContent from './menu';
+import historyContent from './history';
 
 homePage();
 navButton();
 menuContent();
+historyContent();
