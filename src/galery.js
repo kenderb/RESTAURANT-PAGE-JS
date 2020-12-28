e57d75826d3c00dfaa788d404867777b29d5811c
kenderb <40863532+kenderb@users.noreply.github.com>
2020-12-28 18:42:59 -0500

    Add the galery to the app

---
+++ src/galery.js
@@ -0,0 +1,28 @@
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+const images = importAll(require.context('./images/galery/', false, /\.(png|jpg|svg)$/));
+const galeryContent = () => {
+  const container = document.getElementById('container');
+  const galeryDiv = document.createElement('div');
+  galeryDiv.classList.add('contentInfo');
+  galeryDiv.classList.add('galeryInfo');
+  function addGaleryImage(route) {
+    const image = document.createElement('img');
+    image.src = route;
+    image.classList.add('galeryImage');
+    galeryDiv.appendChild(image);
+  }
+
+  images.forEach(element => {
+    addGaleryImage(element.default);
+  });
+  container.appendChild(galeryDiv);
+
+  const content = document.querySelectorAll('.contentInfo');
+  content[3].style.display = 'none';
+  return content[3];
+};
+export default galeryContent;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import homePage from './home';
 import navButton from './button-actions';
 import menuContent from './menu';
 import historyContent from './history';
+import galeryContent from './galery';
 
 homePage();
 navButton();
 menuContent();
 historyContent();
+galeryContent();
